9c5755d659fa97ba4896117bf391c09260619cf6
Arslan <arslanastral@outlook.com>
2021-05-31 17:22:04 +0800

    added transition to button active mode

--- 03_Front-End-Development-Libraries/04_Calculator/src/components/Button.js
+++ 03_Front-End-Development-Libraries/04_Calculator/src/components/Button.js
@@ -13,6 +13,12 @@ const CalculatorButton = styled.button`
   width: 63px;
   height: ${(props) => (props.gridarea === "equal" ? "100%" : "63px")};
   background-color: ${(props) => (props.gridarea === "equal" ? "yellow" : "")};
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  transition: all ease-in-out 0.07s;
+
+  &:active {
+    transform: scale(0.9);
+  }
 `;
 
 const Button = ({ name, gridarea }) => {
