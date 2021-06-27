aecfb6f4ec820a1ef6f51f5cf76161ffca7aaa94
Arslan <arslanastral@outlook.com>
2021-06-27 14:59:12 +0800

    added components to clockboard

--- 03_Front-End-Development-Libraries/05_25+5 Clock/src/components/ClockBoard.jsx
+++ 03_Front-End-Development-Libraries/05_25+5 Clock/src/components/ClockBoard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import MainClock from "./MainClock";
 import Timers from "./Timers";
+import Stats from "./Stats";
+import ControlPanel from "./ControlPanel";
 
 const ClockContainer = styled.div`
   width: min(34rem, 90vw);
   height: max(45rem, 90vh);
-
   background: linear-gradient(
     180deg,
     rgba(22, 46, 252, 0.8) 4.69%,
@@ -19,6 +20,8 @@ const ClockBoard = () => {
     <ClockContainer>
       <MainClock />
       <Timers />
+      <Stats />
+      <ControlPanel />
     </ClockContainer>
   );
 };
